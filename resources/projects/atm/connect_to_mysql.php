7ada283e435f9267fc96b1c956c0a3aad6a71859
Juan Alcantara <juan231f@yahoo.com>
2018-09-18 18:57:07 -0400

    started from scracth

--- resources/projects/atm/connect_to_mysql.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect("localhost", "AtmDatabase", "Y@nkees123") or die(mysql_error());
-mysql_select_db("AtmDatabase") or die(mysql_error());
-?>
--- resources/projects/ng-trailers/js/directives/trailer.js
+++
@@ -1,9 +0,0 @@
-app.directive('trailer', function() { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-      info: '=' 
-    }, 
-    templateUrl: 'js/directives/trailer.html' 
-  }; 
-});
