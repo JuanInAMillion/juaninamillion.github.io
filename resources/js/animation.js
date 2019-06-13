afcb3dfbc6dbee5aa8acbe898d9a2ed90c419fcd
Juan A Alcantara <juan231f@yahoo.com>
2019-06-13 19:59:56 -0400

    tweaked the animation transitions

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -1,27 +1,37 @@
 //ScrollMagic
-var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl = new TimelineMax({
+  onUpdate: updatePercentage
+});
 var tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
-tl.from('.skill-icons', .5, {x:200, width: .8, ease: Power4.easeInOut}, "=-1");
+tl.from('.skill-icons', .5, {
+  x: 100,
+  scale: .1,
+  ease: Power0.easeIn
+}, "=-1");
 
-tl2.from('.project-container', 2, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
+tl2.from('.project-container', 2, {
+  x: -200,
+  opacity: 0,
+  ease: Power0.easeInOut
+}, "=-1");
 
 const scene = new ScrollMagic.Scene({
-  triggerElement: ".skill-section",
-  triggerHook: "onEnter",
-  duration: "100%"
-})
+    triggerElement: ".skill-section",
+    triggerHook: "onEnter",
+    duration: "100%"
+  })
   .setTween(tl)
-		.addTo(controller);
+  .addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
-  triggerElement: "#portfolio",
-  triggerHook: "onEnter",
-  duration: "100%"
-})
+    triggerElement: "#portfolio",
+    triggerHook: "onEnter",
+    duration: "100%"
+  })
   .setTween(tl2)
-		.addTo(controller);
+  .addTo(controller);
 
 
 function updatePercentage() {
