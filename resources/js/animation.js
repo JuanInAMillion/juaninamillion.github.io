162b050a75e77c69235ffa546e7fe77b9d244e0b
Juan A Alcantara <juan231f@yahoo.com>
2019-07-12 21:16:34 -0400

    cleaned up the style.scss and media.scss

--- resources/js/animation.js
+++ resources/js/animation.js
@@ -1,10 +1,10 @@
-let contact = document.getElementById("contact");
+let profile = document.getElementById("profile");
 let scrollButton = document.getElementById("scroll-button");
 let portfolio = document.getElementById("portfolio");
-//MAKES #CONTACT FADE IN AND FOCUS ON WINDOW LOAD
+//MAKES #profile FADE IN AND FOCUS ON WINDOW LOAD
 window.onload = () => {
-  contact.classList.add("appear");
-  contact.style.opacity = "1";
+  profile.classList.add("appear");
+  profile.style.opacity = "1";
 }
 //BOUNCING HEXAGON MADE INTO SCROLL-BUTTON
 function scroll() {
